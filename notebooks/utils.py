e30e31f7f084d1473dfd8ebcfe80230184f372b4
Muthumayan Madhayyan <muthumayan@Muthumayans-MBP.hsd1.ca.comcast.net>
2024-03-30 12:01:10 -0700

    moving common util functions to utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -51,3 +51,65 @@ def generate_splits(classes, directory, split_file="../data/processed/split_defi
     print("\tNumber of test files:", len(test_files))
     
     return train_files, val_files, test_files
+
+
+
+def create_gaussian_filter(rows, cols, sigma):
+    # Create Gaussian Filter: Low Pass Filter
+    M,N = rows, cols
+    H = np.zeros((M,N), dtype=np.float32)
+    D0 = sigma
+    for u in range(M):
+        for v in range(N):
+            D = np.sqrt((u-M/2)**2 + (v-N/2)**2)
+            H[u,v] = np.exp(-D**2/(2*D0*D0))
+
+    return H
+
+
+def fft_image(image):
+
+    # Read the image
+    # image = plt.imread(image)
+
+    # Convert the image to grayscale if it's not already
+    if len(image.shape) > 2:
+        image_gray = np.mean(image, axis=2)
+    else:
+        image_gray = image
+
+    ydim, xdim = image_gray.shape
+    win = np.outer(np.hanning(ydim),np.hanning(xdim))
+    win = win/np.mean(win)
+    
+    # Compute the 2D FFT of the grayscale image
+    fft_image = np.fft.fft2(image_gray*win)
+
+    # Shift the zero frequency component to the center
+    fft_image_shifted = np.fft.fftshift(fft_image)
+
+    # Compute the magnitude spectrum (absolute value) of the shifted FFT
+    magnitude_spectrum = np.abs(fft_image_shifted)
+    
+    return magnitude_spectrum
+
+def apply_gaussian_filter(image, filt):
+    # Convert the image to grayscale if it's not already
+    if len(image.shape) > 2:
+        image_gray = np.mean(image, axis=2)
+    else:
+        image_gray = image
+
+    # Compute the 2D FFT of the image
+    fft_image = np.fft.fft2(image_gray)
+    fft_shifted = np.fft.fftshift(fft_image)
+
+    # Apply the mask to the frequency domain representation
+    fft_shifted_filtered = fft_shifted * filt
+    
+    # Compute the inverse 2D FFT
+    ifft_shifted_filtered = np.fft.ifftshift(fft_shifted_filtered)
+    filtered_image = np.fft.ifft2(ifft_shifted_filtered)
+    filtered_image = np.abs(filtered_image)
+
+    return filtered_image.astype(np.uint8)
